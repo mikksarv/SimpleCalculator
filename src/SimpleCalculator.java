69aeb2cda14298adde1c69780cf2352ae5d61171
mikksarv <mikksrv@gmail.com>
2025-05-21 15:04:53 +0300

    Added addition logic

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class SimpleCalculator {
 
+    public static int add(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter first number");
+
+        Scanner input = new Scanner(System.in);
+        int firstNumber = input.nextInt();
+
+        System.out.println("Enter second number");
+        int secondNumber = input.nextInt();
+
+        System.out.println("The total of the numbers is: " + add(firstNumber, secondNumber) );
+
+    }
 }
