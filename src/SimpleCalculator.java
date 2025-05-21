d2801c1d351458397d5200031498b44ff03f573c
mikksarv <mikksrv@gmail.com>
2025-05-21 16:17:50 +0300

    Added multiplication and division

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class SimpleCalculator {
@@ -9,28 +8,33 @@ public class SimpleCalculator {
     public static int subtract(int firstNumber, int secondNumber){
         return firstNumber - secondNumber;
     }
+    public static int multiply(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+    public static double divide(int firstNumber, int secondNumber){
+        return (double) firstNumber / secondNumber;
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter first number");
         int firstNumber = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Choose the operation +,-,* or / to continue");
+        String operator = input.nextLine();
 
         System.out.println("Enter second number");
         int secondNumber = input.nextInt();
-        input.nextLine();
 
-        System.out.println("Choose the operation + or - to continue");
-        String operator = input.nextLine();
-
-        if(Objects.equals(operator, "+")){
-            System.out.println("The answer is: " + add(firstNumber, secondNumber));
-        }
-        else if (Objects.equals(operator, "-")){
-            System.out.println("The answer is: " + subtract(firstNumber, secondNumber));
-        }
-        else {
-            System.out.println("You did not enter valid operator or number");
+        switch (operator) {
+            case "+" -> System.out.println("The answer is: " + add(firstNumber, secondNumber));
+            case "-" -> System.out.println("The answer is: " + subtract(firstNumber, secondNumber));
+            case "*" -> System.out.println("The answer is: " + multiply(firstNumber, secondNumber));
+            case "/" -> System.out.println("The answer is: " + divide(firstNumber, secondNumber));
+            default -> System.out.println("Something is missing!");
+            }
         }
     }
-}
+
