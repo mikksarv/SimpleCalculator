9fdb7fffa0e5da0e1c1ad33e6f56ee1eb5396477
mikksarv <mikksrv@gmail.com>
2025-05-21 15:52:49 +0300

    Added subtraction logic

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class SimpleCalculator {
@@ -5,17 +6,31 @@ public class SimpleCalculator {
     public static int add(int firstNumber, int secondNumber){
         return firstNumber + secondNumber;
     }
+    public static int subtract(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Enter first number");
-
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number");
         int firstNumber = input.nextInt();
 
         System.out.println("Enter second number");
         int secondNumber = input.nextInt();
+        input.nextLine();
 
-        System.out.println("The total of the numbers is: " + add(firstNumber, secondNumber) );
+        System.out.println("Choose the operation + or - to continue");
+        String operator = input.nextLine();
 
+        if(Objects.equals(operator, "+")){
+            System.out.println("The answer is: " + add(firstNumber, secondNumber));
+        }
+        else if (Objects.equals(operator, "-")){
+            System.out.println("The answer is: " + subtract(firstNumber, secondNumber));
+        }
+        else {
+            System.out.println("You did not enter valid operator or number");
+        }
     }
 }
